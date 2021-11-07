96b0b49b7762f0663b74dc76a73252e30da06cd6
jonatas-gama <jonatasgama@rssolucoesconsultoria.com>
2021-11-07 18:54:17 -0300

    gitignore

---
+++ application/logs/log-2021-11-07.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-11-07 22:48:12 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): Nenhuma conexão pôde ser feita porque a máquina de destino as recusou ativamente C:\xampp\htdocs\form-covid\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2021-11-07 22:48:12 --> Unable to connect to the database
