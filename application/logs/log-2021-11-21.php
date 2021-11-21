9d3e82a5d703e4718b46db122c1d63f5339dcdbc
jonatas-gama <jonatasgama@rssolucoesconsultoria.com>
2021-11-21 17:57:03 -0300

    retirado o campo de imc e separado o form para os dois logins

--- application/logs/log-2021-11-21.php
+++ application/logs/log-2021-11-21.php
@@ -14,3 +14,9 @@ ERROR - 2021-11-21 15:25:12 --> Severity: Warning --> require(/vivian/main): Fai
 ERROR - 2021-11-21 15:25:12 --> Severity: error --> Exception: Failed opening required '/vivian/main' (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\etpocovid\application\views\vivian\create.php 10
 ERROR - 2021-11-21 15:26:41 --> Severity: Warning --> Undefined variable $vivian C:\xampp\htdocs\etpocovid\application\views\vivian\index.php 74
 ERROR - 2021-11-21 15:26:41 --> Severity: Warning --> foreach() argument must be of type array|object, null given C:\xampp\htdocs\etpocovid\application\views\vivian\index.php 74
+ERROR - 2021-11-21 16:06:05 --> Severity: Warning --> Undefined variable $chart_data C:\xampp\htdocs\form-covid\application\views\administracao\template.php 565
+ERROR - 2021-11-21 16:06:05 --> Severity: Warning --> Undefined variable $chart_line C:\xampp\htdocs\form-covid\application\views\administracao\template.php 623
+ERROR - 2021-11-21 16:06:48 --> Severity: Warning --> Undefined variable $chart_data C:\xampp\htdocs\form-covid\application\views\administracao\template.php 565
+ERROR - 2021-11-21 16:06:48 --> Severity: Warning --> Undefined variable $chart_line C:\xampp\htdocs\form-covid\application\views\administracao\template.php 623
+ERROR - 2021-11-21 21:44:46 --> Severity: Warning --> Undefined variable $chart_data C:\xampp\htdocs\form-covid\application\views\administracao\template.php 565
+ERROR - 2021-11-21 21:44:46 --> Severity: Warning --> Undefined variable $chart_line C:\xampp\htdocs\form-covid\application\views\administracao\template.php 623
